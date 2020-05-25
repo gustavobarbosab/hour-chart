832dfac67b7d02c698569c255cb476fbeb74a57a
Gustavo Barbosa <tqi_gbarreto@uolinc.com>
2020-05-25 18:57:20 -0300

    adicionando legendas

--- app/src/main/java/com/gustavobarbosab/hourchart/MainActivity.kt
+++ app/src/main/java/com/gustavobarbosab/hourchart/MainActivity.kt
@@ -3,8 +3,9 @@ package com.gustavobarbosab.hourchart
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
+import kotlinx.android.synthetic.main.activity_main.btChangeChart
+import kotlinx.android.synthetic.main.activity_main.hourChart
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,11 +14,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        hourChart.setup(160, 20)
+        hourChart.setup(20)
         btChangeChart.setOnClickListener {
             hourChart.workedColorResource = randomColor()
             hourChart.missingColorResource = randomColor()
-            hourChart.strokeWidth += 10
+            hourChart.setup(rnd.nextInt(199))
         }
     }
 
--- hourchart/src/main/java/com/gustavobarbosab/hourchart/HourChartView.kt
+++ hourchart/src/main/java/com/gustavobarbosab/hourchart/HourChartView.kt
@@ -4,12 +4,12 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
 import android.graphics.RectF
 import android.os.Parcelable
 import android.util.AttributeSet
 import android.view.View
 import kotlinx.android.parcel.Parcelize
-import java.lang.IllegalArgumentException
 
 class HourChartView @JvmOverloads constructor(
     context: Context,
@@ -18,27 +18,36 @@ class HourChartView @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
 
     private var chartRect = RectF()
+    private var textBounds = Rect()
 
     private val workedPaint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val missingPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val textPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val circleLegendPaint = Paint(Paint.ANTI_ALIAS_FLAG)
 
-    private var totalHours = 160
+    private var totalHours = TOTAL_HOURS_DEFAULT
     private var workedHours = 0
+    private val missingHours
+        get() = totalHours - workedHours
 
     var strokeWidth = STROKE_WIDTH_DEFAULT
         set(value) {
             field = value
-            workedPaint.strokeWidth = value
-            missingPaint.strokeWidth = value
+            workedPaint.strokeWidth = strokeWidthPx
+            missingPaint.strokeWidth = strokeWidthPx
             invalidate()
         }
-    var workedColorResource = Color.GREEN
+    private val strokeWidthPx
+        get() = toPx(strokeWidth)
+
+    var workedColorResource = 0xff54c47f.toInt()
         set(value) {
             field = value
             workedPaint.color = value
             invalidate()
         }
-    var missingColorResource = Color.RED
+
+    var missingColorResource = 0xFFEB9191.toInt()
         set(value) {
             field = value
             missingPaint.color = value
@@ -49,15 +58,21 @@ class HourChartView @JvmOverloads constructor(
         workedPaint.apply {
             color = workedColorResource
             style = Paint.Style.STROKE
-            strokeWidth = this@HourChartView.strokeWidth
+            strokeWidth = this@HourChartView.strokeWidthPx
             strokeCap = Paint.Cap.BUTT
         }
         missingPaint.apply {
             color = missingColorResource
             style = Paint.Style.STROKE
-            strokeWidth = this@HourChartView.strokeWidth
+            strokeWidth = this@HourChartView.strokeWidthPx
             strokeCap = Paint.Cap.BUTT
         }
+        textPaint.apply {
+            color = Color.DKGRAY
+            textSize = toPx(TEXT_SIZE_DEFAULT)
+            textAlign = Paint.Align.CENTER
+        }
+        circleLegendPaint.style = Paint.Style.FILL
     }
 
     override fun onSaveInstanceState(): Parcelable? =
@@ -89,6 +104,7 @@ class HourChartView @JvmOverloads constructor(
         super.onDraw(canvas)
         drawMissingHours(canvas)
         drawWorkedHours(canvas)
+        drawLegend(canvas)
     }
 
     private fun drawWorkedHours(canvas: Canvas) {
@@ -101,28 +117,68 @@ class HourChartView @JvmOverloads constructor(
         canvas.drawArc(chartRect, 0f, 360f, false, missingPaint)
     }
 
+    private fun drawLegend(canvas: Canvas) {
+        val centerX = chartRect.centerX()
+        val centerY = chartRect.centerY()
+
+        val textSize = toPx(TEXT_SIZE_DEFAULT)
+        val halfTextSize = textSize / 2
+
+        val workedY = centerY - halfTextSize
+        val missingY = centerY + textSize
+        val marginEndCircleToText = toPx(4f)
+
+        // Draw worked legend
+        val workedString = "+${workedHours}h"
+        canvas.drawText(
+            workedString,
+            centerX,
+            workedY,
+            textPaint
+        )
+        textPaint.getTextBounds(workedString, 0, workedString.length, textBounds)
+        circleLegendPaint.color = workedColorResource
+        canvas.drawCircle(
+            centerX - ((textBounds.width() / 2) + halfTextSize + marginEndCircleToText),
+            workedY + textBounds.exactCenterY(),
+            halfTextSize,
+            circleLegendPaint
+        )
+
+        // Draw missing legend
+        val missingString = "-${missingHours}h"
+        canvas.drawText(missingString, centerX, missingY, textPaint)
+        textPaint.getTextBounds(missingString, 0, missingString.length, textBounds)
+        circleLegendPaint.color = missingColorResource
+        canvas.drawCircle(
+            centerX - ((textBounds.width() / 2) + halfTextSize + marginEndCircleToText),
+            missingY + textBounds.exactCenterY(),
+            halfTextSize,
+            circleLegendPaint
+        )
+    }
+
     private fun convertHoursToAngle(hours: Int): Float = (hours * 360f) / totalHours
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        changeChartRect(w, h)
+        val startChart = paddingStart + PADDING_DEFAULT
+        val topChart = paddingTop + PADDING_DEFAULT
+        val endChart = w.toFloat() - (paddingEnd + PADDING_DEFAULT)
+        val bottomChart = h.toFloat() - (paddingBottom + PADDING_DEFAULT)
+        chartRect.set(startChart, topChart, endChart, bottomChart)
     }
 
-    private fun changeChartRect(w: Int, h: Int) {
-        val start = paddingStart + PADDING_DEFAULT
-        val top = paddingTop + PADDING_DEFAULT
-        val end = w.toFloat() - (paddingEnd + PADDING_DEFAULT)
-        val bottom = h.toFloat() - (paddingBottom + PADDING_DEFAULT)
-
-        chartRect.set(start, top, end, bottom)
-    }
-
-    fun setup(totalHours: Int, workedHours: Int) {
+    fun setup(workedHours: Int, totalHours: Int = TOTAL_HOURS_DEFAULT) {
         if (workedHours > totalHours) throw IllegalArgumentException()
         this.totalHours = totalHours
         this.workedHours = workedHours
         invalidate()
     }
+
+    private fun toPx(dp: Float) = dp * context.resources.displayMetrics.density
+
+    private fun toDp(px: Float) = px / context.resources.displayMetrics.density
 
     @Parcelize
     data class ViewState(
@@ -135,7 +191,9 @@ class HourChartView @JvmOverloads constructor(
     ) : Parcelable
 
     companion object {
+        const val TOTAL_HOURS_DEFAULT = 200
         const val PADDING_DEFAULT = 60F
-        const val STROKE_WIDTH_DEFAULT = 60F
+        const val STROKE_WIDTH_DEFAULT = 22F
+        const val TEXT_SIZE_DEFAULT = 16F
     }
 }
